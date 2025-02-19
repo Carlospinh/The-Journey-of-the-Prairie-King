e6978f9dc56b6d642a68d93aa16e3991517afc1e
MarcPladellorensPerez <74015037+MarcPladellorensPerez@users.noreply.github.com>
2025-02-19 15:14:18 +0100

    Player Movment
    Afegits spirites i codi pel moviement del jugador

--- src/main.c
+++ src/main.c
@@ -1,47 +1,122 @@
 #include "raylib.h"
 
-// Definir estados del juego
-typedef enum GameScreen { MENU, GAMEPLAY } GameScreen;
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 720
 
-int main() {
-    // Inicializar ventana
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    InitWindow(screenWidth, screenHeight, "Journey of the Prairie King");
+typedef enum GameState {
+    MENU,
+    PLAYING
+} GameState;
 
-    // Cargar imagen del logo
-    Texture2D logo = LoadTexture("resources/JOPK_logo.png");
-    
-    // Estado del juego
-    GameScreen currentScreen = MENU;
-    
-    while (!WindowShouldClose()) {
-        // Manejo de entrada
-        if (currentScreen == MENU) {
-            if (IsKeyPressed(KEY_ENTER) || IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                currentScreen = GAMEPLAY;  // Ir al juego
+GameState currentGameState = MENU;
+
+Texture2D playerSprites[4];
+Vector2 playerPosition = { SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2 };
+int currentFrame = 1;  // Empieza con player2.png (posición quieta)
+float frameTime = 0.2f;  // Tiempo entre cambios de sprite
+float frameCounter = 0.0f;
+float playerSpeed = 200.0f;
+bool movingLeft = false;
+bool movingRight = false;
+
+Texture2D menuLogo;
+Rectangle startButton = { SCREEN_WIDTH / 2 - 100, SCREEN_HEIGHT - 100, 200, 50 };
+
+void InitGame() {
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Journey of the Prairie King");
+    SetTargetFPS(60);
+
+    menuLogo = LoadTexture("resources/JOPK_logo.png");
+    playerSprites[0] = LoadTexture("resources/player1.png");
+    playerSprites[1] = LoadTexture("resources/player2.png");
+    playerSprites[2] = LoadTexture("resources/player3.png");
+    playerSprites[3] = LoadTexture("resources/player4.png");
+}
+
+void UpdateGame() {
+    if (currentGameState == MENU) {
+        if (IsKeyPressed(KEY_ENTER)) {
+            currentGameState = PLAYING;
+        }
+    } else if (currentGameState == PLAYING) {
+        bool moving = false;
+
+        // Dirección de movimiento
+        if (IsKeyDown(KEY_A) || IsKeyDown(KEY_S)) {
+            movingLeft = true;
+            movingRight = false;
+        } 
+        if (IsKeyDown(KEY_D) || IsKeyDown(KEY_W)) {
+            movingRight = true;
+            movingLeft = false;
+        }
+
+        if (IsKeyDown(KEY_W)) {
+            playerPosition.y -= playerSpeed * GetFrameTime();
+            moving = true;
+        }
+        if (IsKeyDown(KEY_S)) {
+            playerPosition.y += playerSpeed * GetFrameTime();
+            moving = true;
+        }
+        if (IsKeyDown(KEY_A)) {
+            playerPosition.x -= playerSpeed * GetFrameTime();
+            moving = true;
+        }
+        if (IsKeyDown(KEY_D)) {
+            playerPosition.x += playerSpeed * GetFrameTime();
+            moving = true;
+        }
+
+        if (!moving) {
+            currentFrame = 1;  // Si está quieto, usa player2.png
+        } else {
+            // Animación con mismo tiempo de espera para ambos cambios
+            frameCounter += GetFrameTime();
+            if (frameCounter >= frameTime) {
+                frameCounter = 0.0f;
+
+                if (movingLeft) {
+                    currentFrame = (currentFrame == 0) ? 1 : 0;  // Alterna entre player1 y player2
+                } else if (movingRight) {
+                    currentFrame = (currentFrame == 2) ? 3 : 2;  // Alterna entre player3 y player4
+                }
             }
         }
-        
-        // Dibujar
-        BeginDrawing();
-        ClearBackground(BLACK);
-
-        if (currentScreen == MENU) {
-            // Dibujar logo
-            DrawTexture(logo, (screenWidth - logo.width) / 2, 150, WHITE);
-
-            // Dibujar botón de inicio
-            DrawText("Press ENTER to Start", 280, 450, 20, WHITE);
-        }
-        
-        EndDrawing();
     }
-    
-    // Descargar recursos
-    UnloadTexture(logo);
-    
-    // Cerrar ventana
+}
+
+void DrawGame() {
+    BeginDrawing();
+    ClearBackground(BLACK);
+
+    if (currentGameState == MENU) {
+        DrawTexture(menuLogo, (SCREEN_WIDTH - menuLogo.width) / 2, (SCREEN_HEIGHT - menuLogo.height) / 3, WHITE);
+        DrawRectangleRec(startButton, WHITE);
+        DrawText("START", startButton.x + 60, startButton.y + 15, 20, BLACK);
+    } else if (currentGameState == PLAYING) {
+        DrawTexture(playerSprites[currentFrame], playerPosition.x, playerPosition.y, WHITE);
+    }
+
+    EndDrawing();
+}
+
+void CloseGame() {
+    UnloadTexture(menuLogo);
+    for (int i = 0; i < 4; i++) {
+        UnloadTexture(playerSprites[i]);
+    }
     CloseWindow();
+}
+
+int main() {
+    InitGame();
+
+    while (!WindowShouldClose()) {
+        UpdateGame();
+        DrawGame();
+    }
+
+    CloseGame();
     return 0;
 }
